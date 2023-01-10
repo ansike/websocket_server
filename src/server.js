64c6ab89c6fcc3d94f320b294a736b7a93a683cb
ansike <984315765@qq.com>
2023-01-10 09:22:01 +0800

    feat: decode ws data

--- src/server.js
+++ src/server.js
@@ -22,8 +22,49 @@ app.on("upgrade", (req, socket, head) => {
 
   console.log("upgrade");
   socket.on("data", (d) => {
-    // TODO 解析
-    console.log(d);
+    const data = decodeWsData(d);
+    console.log(data.payloadData?.toString());
   });
 });
 app.listen(8080);
+
+function decodeWsData(data) {
+  let start = 0;
+  let frame = {
+    isFinal: (data[start] & 0x80) === 0x80,
+    opcode: data[start++] & 0xf,
+    masked: (data[start] & 0x80) === 0x80,
+    payloadLen: data[start++] & 0x7f,
+    maskingKey: "",
+    payloadData: null,
+  };
+
+  console.log(frame);
+  if (frame.payloadLen === 126) {
+    frame.payloadLen = (data[start++] << 8) + data[start++];
+  } else if (frame.payloadLen === 127) {
+    frame.payloadLen = 0;
+    for (let i = 7; i >= 0; i--) {
+      frame.payloadLen += data[start++] << (i * 8);
+    }
+  }
+  if (frame.payloadLen) {
+    if (frame.masked) {
+      const maskingKey = [
+        data[start++],
+        data[start++],
+        data[start++],
+        data[start++],
+      ];
+      frame.maskingKey = maskingKey;
+      frame.payloadData = data
+        .slice(start, start + frame.payloadLen)
+        .map((byte, idx) => {
+          return byte ^ maskingKey[idx % 4];
+        });
+    } else {
+      frame.payloadData = data.slice(start, start + frame.payloadLen);
+    }
+  }
+  return frame;
+}
